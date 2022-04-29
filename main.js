cc1b2fa423003c8b0889391d6f4228d8c2eb5c1e
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-04-28 20:27:38 -0500

    Maneras de selcionar con JS EMC

---
+++ main.js
@@ -0,0 +1,33 @@
+// ----------------- EcmaScript -----------------
+
+let etiqueta = document.querySelector('.contenido');
+
+etiqueta.insertAdjacentHTML("afterbegin", "<em>Mercedes</em>");
+
+
+
+
+
+
+
+
+
+
+
+// ----------------- Javascript -----------------
+
+
+// let etiqueta = document.getElementsByTagName("h1")[0];
+// let identificador = document.getElementById("MyEti");
+// let clase = document.getElementsByClassName("contenido");
+// let nombre = document.getElementsByName("NombreProfesor");
+
+// console.log(etiqueta.innerHTML);
+// // etiqueta.innerHTML = `<em>Hola</em>`
+// console.log(etiqueta.innerText);
+// // etiqueta.innerText = `<em>Hola</em>`
+// console.log(etiqueta.outerHTML);
+// // etiqueta.outerHTML = `<em>Hola</em>`
+// console.log(etiqueta.outerText);
+// // etiqueta.outerText = `<em>Hola</em>`
+// console.log(etiqueta.textContent);
